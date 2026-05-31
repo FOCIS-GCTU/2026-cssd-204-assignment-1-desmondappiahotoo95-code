e4ac5afe3bcf94d5133f480e108005ed3e3f97ae
desmondappiahotoo95-code <desmondappiahotoo95@gmail.com>
2026-05-31 19:23:59 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+#Assignment Number: 1
+#
+#Name: Desmond Appiah-Otoo
+#STUDENT ID: 2425404386
+#Email: 2425404386@live.gctu.edu.gh
+#Grader: Mr. Augustus
+#
+# On my honor, Appiah-Otoo Desmond, this programming assignment is my own work
+#and i have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    # Print initials D.A.O in block letters and periods
 
+    print()
+    print("...DAO")
+    print()
+    
+    print("...DDDDDDD...................AAA................OOOOOOOOO.........")
+    print("...DDDDDDDDD................AAAAA..............OOOOOOOOOOO........")
+    print("...DD......DDD.............AA...AA.............OO.......OO........")
+    print("...DD.......DD............AA.....AA............OO.......OO........")
+    print("...DD........D...........AAAAAAAAAAA...........OO.......OO........")
+    print("...DD........D...........AAAAAAAAAAA...........OO.......OO........")
+    print("...DD.......DD...........AA.......AA...........OO.......OO........")
+    print("...DD......DDD...........AA.......AA...........OO.......OO........")
+    print("...DDDDDDDDD.......**....AA.......AA.....**....OOOOOOOOOOO.....**.")
+    print("...DDDDDDD.........**....AA.......AA.....**.....OOOOOOOOO......**.")
+    print()
 
 
 main()
